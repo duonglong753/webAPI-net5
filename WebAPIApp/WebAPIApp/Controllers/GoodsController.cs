04e0a8e2d4eb65b72b81caefbae92cdbd889b1a3
duonglong753 <duonglong753@gmail.com>
2022-07-17 18:03:04 +0700

    create first API

---
+++ WebAPIApp/WebAPIApp/Controllers/GoodsController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebAPIApp.Models;
+using System.Linq;
+
+namespace WebAPIApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoodsController : ControllerBase
+    {
+        public static List<Goods> Goods = new List<Goods>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(Goods);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetByID(string id)
+        {
+            try
+            {
+                //LinQ (object) query    
+                var goods = Goods.SingleOrDefault(g => g.GoodsId == Guid.Parse(id));
+                if (goods == null)
+                {
+                    return NotFound();
+                }
+                return Ok(goods);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            
+        }
+        [HttpPost]
+        public IActionResult Create(GoodsVM goodsVM)
+        {
+            var goods = new Goods
+            {
+                GoodsId = Guid.NewGuid(),
+                GoodsName = goodsVM.GoodsName,
+                UnitPrice = goodsVM.UnitPrice
+            };
+            Goods.Add(goods);
+            return Ok(new
+            {
+                Success = true,
+                Data = goods
+            });
+        }
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, Goods goodsEdit)
+        {
+            try
+            {
+                //LinQ (object) query    
+                var goods = Goods.SingleOrDefault(g => g.GoodsId == Guid.Parse(id));
+                if (goods == null)
+                {
+                    return NotFound();
+                }
+                if(goodsEdit.GoodsId.ToString() != id)
+                {
+                    return BadRequest();
+                }
+                //update
+                goods.GoodsName = goodsEdit.GoodsName;
+                goods.UnitPrice = goodsEdit.UnitPrice;
+                return Ok(goods);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Remove(string id)  
+        {
+            try
+            {
+                //LinQ (object) query    
+                var goods = Goods.SingleOrDefault(g => g.GoodsId == Guid.Parse(id));
+                if (goods == null)
+                {
+                    return NotFound();
+                }
+                //delete
+                Goods.Remove(goods);
+                return Ok(goods);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+        }
+    }
+}
---
+++ WebAPIApp/WebAPIApp/Models/Goods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebAPIApp.Models
+{
+    public class GoodsVM
+    {
+        public string GoodsId { get; set; }
+        public string GoodsName { get; set; }
+        public double UnitPrice { get; set; }
+    }
+    public class Goods : GoodsVM
+    {
+        public Guid GoodsId { get; set; }
+    }
+}
